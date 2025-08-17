6e3cddc6f47dfd12d8d9cc133f1f91a3818ce127
Abhishek Jain <aj482522@gmai.com>
2025-08-18 01:00:08 +0530

    first commit

--- create_admin.py
+++ create_admin.py
@@ -6,9 +6,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 django.setup()
 
 User = get_user_model()
-username = os.environ.get('Abhishek')
-email = os.environ.get('aj482522@gmail.com')
-password = os.environ.get('Abhi@2002')
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
 
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username=username, email=email, password=password)
